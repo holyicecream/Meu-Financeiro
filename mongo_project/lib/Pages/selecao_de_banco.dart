85570882d2be97540a36138832af4f18a33ddca3
Thomas Buchser Monteiro <thomas.blendsbr@gmail.com>
2024-06-07 21:49:37 -0300

    Mostra os bancos sem pesquisar

--- mongo_project/lib/Pages/selecao_de_banco.dart
+++ mongo_project/lib/Pages/selecao_de_banco.dart
@@ -42,123 +42,114 @@ class SelecaoDeBancoState extends State<SelecaoDeBanco> {
     );
   }
 
-  @override
-  Widget build(BuildContext context) {
-    count++;
-    print("build selecaoDeBanco $count");
-    try {
-      todosArguments =
-          ModalRoute.of(context)!.settings.arguments as TodosArguments;
-    } catch (e) {
-      // print(e.toString());
-    }
+ @override
+Widget build(BuildContext context) {
+  count++;
+  print("build selecaoDeBanco $count");
+  try {
+    todosArguments = ModalRoute.of(context)!.settings.arguments as TodosArguments;
+  } catch (e) {
+    // print(e.toString());
+  }
 
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        title: const Text(
-          "Seleção de Banco",
-          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-          textAlign: TextAlign.center,
-        ),
+  return Scaffold(
+    appBar: AppBar(
+      centerTitle: true,
+      backgroundColor: Colors.green,
+      title: const Text(
+        "Seleção de Banco",
+        style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(15, 50, 15, 15),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  TextFormField(
-                    controller: pesquisaController,
-                    onChanged: (value) {
-                      if (value.isEmpty) {
+    ),
+    body: SafeArea(
+      child: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(15, 50, 15, 15),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                TextFormField(
+                  controller: pesquisaController,
+                  onChanged: (value) {
+                    if (value.isEmpty) {
+                      dataBancosSearch = dataBancos;
+                    } else {
+                      dataBancosSearch = dataBancos.where((x) => x['banco']
+                          .toLowerCase()
+                          .contains(value.toLowerCase())).toList();
+                    }
+                    setState(() {});
+                  },
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Busque por instituição',
+                    suffixIcon: Icon(Icons.search),
+                  ),
+                ),
+                FutureBuilder(
+                  future: MongoDatabaseBancos.getData(),
+                  builder: (context, AsyncSnapshot snapshot) {
+                    if (snapshot.hasData) {
+                      dataBancos = snapshot.data;
+                      if (pesquisaController.text.isEmpty) {
                         dataBancosSearch = dataBancos;
-                      } else {
-                        dataBancosSearch = [];
-                        dataBancosSearch = dataBancos
-                            .where((x) => x['banco']
-                                .toLowerCase()
-                                .contains(value.toLowerCase()))
-                            .toList();
                       }
-                      setState(() {});
-                    },
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Busque por instituição',
-                      suffixIcon: Icon(Icons.search),
-                    ),
-                  ),
-                  FutureBuilder(
-                    future: MongoDatabaseBancos.getData(),
-                    builder: (context, AsyncSnapshot snapshot) {
-                      if (snapshot.hasData) {
-                        return ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          itemCount: dataBancosSearch.length,
-                          shrinkWrap: true,
-                          itemBuilder: (context, index) {
-                            Widget leadingText;
-                            Widget titleText;
-                            try {
-                              leadingText = Text(dataBancosSearch[index]
-                                      ['banco']
-                                  .toString()
-                                  .split('-')[0]);
-                              titleText = Text(dataBancosSearch[index]['banco']
-                                  .toString()
-                                  .split('-')[1]);
-                            } catch (e) {
-                              print("ali olha $e");
-                              leadingText = Text('');
-                              titleText = Text('');
-                            }
-                            return Padding(
-                              padding: EdgeInsets.all(5),
-                              child: Card(
-                                child: ListTile(
-                                  iconColor: Colors.black,
-                                  onTap: () {
-                                    todosArguments.dataBancos.codBanco =
-                                        dataBancosSearch[index]['cod_banco'];
-                                    todosArguments.dataBancos.banco =
-                                        dataBancosSearch[index]['banco'];
-                                    todosArguments.dataExtrato.codBanco =
-                                        dataBancosSearch[index]['cod_banco'];
-                                    todosArguments.dataBancosUsuario.codBanco =
-                                        dataBancosSearch[index]['cod_banco'];
-                                    Navigator.pushReplacementNamed(
-                                        context, '/Main',
-                                        arguments: todosArguments);
-                                  },
-                                  leading: leadingText,
-                                  title: titleText,
-                                ),
+                      return ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        itemCount: dataBancosSearch.length,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          Widget leadingText;
+                          Widget titleText;
+                          try {
+                            leadingText = Text(dataBancosSearch[index]['banco']
+                                .toString()
+                                .split('-')[0]);
+                            titleText = Text(dataBancosSearch[index]['banco']
+                                .toString()
+                                .split('-')[1]);
+                          } catch (e) {
+                            print("ali olha $e");
+                            leadingText = const Text('');
+                            titleText = const Text('');
+                          }
+                          return Padding(
+                            padding: const EdgeInsets.all(5),
+                            child: Card(
+                              child: ListTile(
+                                iconColor: Colors.black,
+                                onTap: () {
+                                  todosArguments.dataBancos.codBanco =
+                                      dataBancosSearch[index]['cod_banco'];
+                                  todosArguments.dataBancos.banco =
+                                      dataBancosSearch[index]['banco'];
+                                  todosArguments.dataExtrato.codBanco =
+                                      dataBancosSearch[index]['cod_banco'];
+                                  todosArguments.dataBancosUsuario.codBanco =
+                                      dataBancosSearch[index]['cod_banco'];
+                                  Navigator.pushReplacementNamed(context, '/Main',
+                                      arguments: todosArguments);
+                                },
+                                leading: leadingText,
+                                title: titleText,
                               ),
-                            );
-                          },
-                        );
-                      } else {
-                        return Container();
-                      }
-                    },
-                  ),
-                  // Padding(
-                  //   padding: EdgeInsets.all(15),
-                  //   child: ListTile(
-                  //     leading: Text('leadingText'),
-                  //     title: Text('titleText'),
-                  //   ),
-                  // ),
-                ],
-              ),
+                            ),
+                          );
+                        },
+                      );
+                    } else {
+                      return const CircularProgressIndicator();
+                    }
+                  },
+                ),
+              ],
             ),
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
 }
